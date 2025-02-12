6c6722cb172196b9b73eb098604a30a64566f6b0
santelizvargas <ushija6@gmail.com>
2025-02-11 21:40:13 -0600

    feat: Add project

---
+++ SwiftUI-Coordinator/SwiftUI_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoordinatorApp.swift
+//  SwiftUI-Coordinator
+//
+//  Created by Steven Santeliz on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
